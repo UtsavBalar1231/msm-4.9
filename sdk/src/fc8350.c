eb011f6489e93c52beb9cd1c91d69f9a7cecb1fb
amarenr <amarenr@motorola.com>
2019-09-20 22:23:33 +0530

    Revert "(CR):DTV modify fc8350 power sequence DTV"
    The fc8350 power on power off sequence modification done for Doha
    are having side-effects and affecting for Ocean DTV functionality.
    
    Hence revert this patch.
    
    This reverts commit 24ff815503b612cacdd9274289b5e0f837234d3d
    
    Change-Id: I1643fc4802d27371bf383d9071d40e92dfd4db21
    Reviewed-on: https://gerrit.mot.com/1425776
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- sdk/src/fc8350.c
+++ sdk/src/fc8350.c
@@ -239,9 +239,8 @@ void isdbt_hw_init(void)
 {
 	mutex_lock(&driver_mode_lock);
 	print_log(0, "isdbt_hw_init\n");
-	gpio_set_value(GPIO_ISDBT_RST, 0);
 	gpio_set_value(GPIO_ISDBT_LDO, 1);
-	mdelay(20);
+	gpio_set_value(GPIO_ISDBT_RST, 0);
 	gpio_set_value(GPIO_ISDBT_PWR_EN, 1);
 	mdelay(5);
 	gpio_set_value(GPIO_ISDBT_RST, 1);
@@ -258,10 +257,8 @@ void isdbt_hw_deinit(void)
 {
 	mutex_lock(&driver_mode_lock);
 	print_log(0, "isdbt_hw_deinit\n");
-	gpio_set_value(GPIO_ISDBT_PWR_EN, 0);
-	mdelay(1);
 	gpio_set_value(GPIO_ISDBT_RST, 0);
-	mdelay(1);
+	gpio_set_value(GPIO_ISDBT_PWR_EN, 0);
 	gpio_set_value(GPIO_ISDBT_LDO, 0);
 	driver_mode = ISDBT_POWEROFF;
 	mutex_unlock(&driver_mode_lock);
